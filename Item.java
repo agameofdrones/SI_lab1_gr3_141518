e1d01d973e0caac7deb9ae390a8f3973d2c31632
Igor Grubikj <igor.grubikj@alite-international.com>
2020-04-12 01:15:53 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -6,7 +6,44 @@ class Item {
 	int taxType;
 
 	// TODO constructor
+	public Item(int id, String name, double price, int taxType) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.taxType = taxType;
+	}
 
+	int getId() {
+		return id;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	double getPrice() {
+		return price;
+	}
+
+	int getTaxType() {
+		return taxType;
+	}
+
+	int setId(int id) {
+		return this.id = id;
+	}
+
+	String setName(String name) {
+		return this.name = name;
+	}
+
+	double setPrice(double price) {
+		return this.price = price;
+	}
+
+	int setTaxType(int taxType) {
+		return this.taxType = taxType;
+	}
 	// TODO setters and getters
 
 	double taxReturn() {
